e9d9174cb2fbe1ba942005c86bfe5cb30dcaa1b4
Jiaqi Su <fromsujiaqi@gmail.com>
2019-10-18 08:11:58 +0000

    batch run

--- run_experiments.m
+++ run_experiments.m
@@ -1,18 +1,9 @@
-ftoread = 'speech_dict_2570_all.csv';
+ftoread = 'speech_dict_2570.csv';
 S = csvread(ftoread);
 
-ftoread = 'noise_dict_2570_all.csv';
+ftoread = 'noise_dict_2570.csv';
 N = csvread(ftoread);
 
-ftoread = './noise_dict/noise_dict_1000_ipad_office1.csv';
-AN = csvread(ftoread);
-
-ftoread = './test_dict/test_dict_2570_f10_script5_ipad_office1.csv';
-Z = csvread(ftoread);
-sub_Z = Z(:, 10:110)
-
-A = [S N AN];
-size(A)
 L = 10;
 lambda_s = 1.6;
 lambda_n = 0.8;
@@ -22,4 +13,22 @@ numiter = 100;
 updateH = 1;
 computecost = 1;
 
-[activations,H,cost] = NMDwithReverb(sub_Z, A, L, Lambda, numspeechexemplars, numiter, updateH, computecost)
+envs = ['ipad_office1', 'ipad_balcony1', 'ipad_confroom1', 'ipad_confroom2', 'ipad_bedroom1', 
+        'ipad_livingroom1', 'ipad_office2', 'ipadflat_confroom1', 'ipadflat_office1', 'iphone_balcony1', 
+        'iphone_bedroom1', 'iphone_livingroom1']
+
+for i =1:13
+    env = envs(i)
+    ftoread = './noise_dict/noise_dict_1000_'+env+'.csv';
+    AN = csvread(ftoread);
+
+    ftoread = './test_dict/test_dict_2570_f10_script5_'+env+'.csv';
+    Z = csvread(ftoread);
+    sub_Z = Z(:, 10:110);
+
+    A = [S N AN];
+    size(A)
+
+    [activations,H,cost] = NMDwithReverb(sub_Z, A, L, Lambda, numspeechexemplars, numiter, updateH, computecost);
+    csvwrite("test_rir_f10_ipad_office1.csv", H);
+end
