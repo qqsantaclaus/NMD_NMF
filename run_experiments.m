838cef03bf196651ff5fcdc97ee8ec5860095aec
Jiaqi Su <fromsujiaqi@gmail.com>
2019-10-18 08:16:05 +0000

    debug

--- run_experiments.m
+++ run_experiments.m
@@ -13,12 +13,12 @@ numiter = 100;
 updateH = 1;
 computecost = 1;
 
-envs = ['ipad_office1', 'ipad_balcony1', 'ipad_confroom1', 'ipad_confroom2', 'ipad_bedroom1', 
+envs = {'ipad_office1', 'ipad_balcony1', 'ipad_confroom1', 'ipad_confroom2', 'ipad_bedroom1', 
         'ipad_livingroom1', 'ipad_office2', 'ipadflat_confroom1', 'ipadflat_office1', 'iphone_balcony1', 
-        'iphone_bedroom1', 'iphone_livingroom1']
+        'iphone_bedroom1', 'iphone_livingroom1'}
 
 for i =1:13
-    env = envs(i)
+    env = envs{i}
     ftoread = './noise_dict/noise_dict_1000_'+env+'.csv';
     AN = csvread(ftoread);
 
